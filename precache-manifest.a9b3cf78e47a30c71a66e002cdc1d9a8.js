b3b22179c9c6cf7aeaf6cab251a72d9b1ab0d7b5
8jesuspinniusharani <pinni.usha44@gmail.com>
2022-10-06 22:48:34 +0530

    Updates

---
+++ precache-manifest.a9b3cf78e47a30c71a66e002cdc1d9a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93abcf06b0d770a35452",
+    "url": "/usha-rani/static/css/main.ca135d8c.chunk.css"
+  },
+  {
+    "revision": "93abcf06b0d770a35452",
+    "url": "/usha-rani/static/js/main.93abcf06.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/usha-rani/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/usha-rani/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "a7d2277d88c08f60b636",
+    "url": "/usha-rani/static/js/runtime~main.a7d2277d.js"
+  },
+  {
+    "revision": "1854ec0b19b0ddd60e399d1c0aa99d48",
+    "url": "/usha-rani/index.html"
+  }
+];
